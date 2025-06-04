0e77d397b60546f0e8c7fbfee4fd26431389993e
sam <sam.bai@berkeley.edu>
2025-06-04 12:45:18 -0700

    tried with tries

--- medium/211DesignAddandSearchWordsDataStructure.py
+++ medium/211DesignAddandSearchWordsDataStructure.py
@@ -1,52 +1,39 @@
-from typing import List
-
-
 class Node:
-    def __init__(self, val, end, word):
+    def __init__(self, val, end):
         self.val = val
         self.end = end
-        self.word = word
         self.next = {}
 
-class Solution:
-    def findWords(board: List[List[str]], words: List[str]) -> List[str]:
-        root = Node(-1, False, "")
-        for i in words:
-            base = root
-            for j in i:
-                if j in base.next:
-                    base = base.next[j]
-                else:
-                    curr = base.word
-                    base.next[j] = Node(j, False, curr + j)
-                    base = base.next[j]
-            base.end = True
 
-        visited = set()        
-        ans = set()
-        def dfs(x, y, curr):
-            if (x, y) in visited or x < 0 or x >= len(board) or y < 0 or y >= len(board[0]) or (board[x][y] not in curr.next):
-                return
-            if board[x][y] in curr.next and curr.next[board[x][y]].end:
-                ans.add(curr.next[board[x][y]].word)
-                
-            visited.add((x, y))
-            dfs(x + 1, y, curr.next[board[x][y]])
-            dfs(x - 1, y, curr.next[board[x][y]])
-            dfs(x, y + 1, curr.next[board[x][y]])
-            dfs(x, y - 1, curr.next[board[x][y]])
-            visited.remove((x, y))
+class WordDictionary:
+
+    def __init__(self):
+        self.root = Node(-1, False)
+
+    def addWord(self, word):
+        base = self.root
+
+        for i in word:
+            if i in base.next:
+                base = base.next[i]
+            else:
+                base.next[i] = Node(i, False)
+                base = base.next[i]
+        base.end = True
+
+    def search(self, word):
+        base = self.root
         
-        for i in range(len(board)):
-            for j in range(len(board[0])):
-                dfs(i, j, root)
-        return ans     
+        def bfs(curr, word):
+            if not word and curr.end:
+                return True
+            elif not word:
+                return False
 
-board = [
-  ["a","b","c","d"],
-  ["s","a","a","t"],
-  ["a","c","k","e"],
-  ["a","c","d","n"]
-]
-words = ["bat","cat","back","backend","stack"]
-print(Solution.findWords(board, words))
+            ans = False
+            for i, j in curr.next.items():
+                if i == word[0] or word[0] == ".":
+                    ans = ans or bfs(j, word[1:])
+            return ans
+
+        return bfs(base, word)
