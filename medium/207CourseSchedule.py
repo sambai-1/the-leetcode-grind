9c6ea6c041f67a2277573082109142b76ee4eb88
sam <sam.bai@berkeley.edu>
2025-06-10 11:21:20 -0700

    imo bad problem

---
+++ medium/207CourseSchedule.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def canFinish(self, numCourses, prerequisites):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        prereqs = defaultdict(list)
+        for i in prerequisites:
+            x, y = i
+            prereqs[x].append(y)
+        
+        visited = set()
+        def dfs(curr):
+            if curr in visited:
+                return False
+            if curr not in prereqs:
+                return True
+            visited.add(curr)
+            for i in prereqs[curr]:
+                if not dfs(i):
+                    return False
+            visited.remove(curr)
+            return True
+
+        for i, j in prereqs.items():
+            if not dfs(i):
+                return False
+        return True
