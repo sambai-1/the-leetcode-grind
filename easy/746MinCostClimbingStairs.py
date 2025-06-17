3b5701bc425bdb187e16665c6d8873b87cb6352d
sam <sam.bai@berkeley.edu>
2025-06-17 09:38:12 -0700

    the goat

---
+++ easy/746MinCostClimbingStairs.py
@@ -0,0 +1,10 @@
+#lowkey one of my favorite problems
+from typing import List
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        for i in range(len(cost) - 3, -1, -1):
+            cost[i] = cost[i] + min(cost[i + 1], cost[i + 2])
+        return min(cost[0], cost[1])
+            
