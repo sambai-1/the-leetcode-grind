8344b55c53c4ed99f5407b74c022d5cd3ddbc812
sam <sam.bai@berkeley.edu>
2025-06-19 13:00:12 -0700

    basic dp

---
+++ medium/62UniquePaths.py
@@ -0,0 +1,7 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[1] * n for _ in range(m)]
+        for i in range(m - 2, -1, -1):
+            for j in range(n-2, -1, -1):
+                grid[i][j] = grid[i][j+1] + grid[i + 1][j]
+        return grid[0][0]        
