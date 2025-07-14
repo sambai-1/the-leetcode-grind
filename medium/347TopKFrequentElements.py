e7b59cf49208cb1a8e846c1ded9402ae7d0b73b0
sam <sam.bai@berkeley.edu>
2025-07-14 09:49:29 -0700

    1000% speed memory upgrade

--- medium/347TopKFrequentElements.py
+++ medium/347TopKFrequentElements.py
@@ -1,26 +1,24 @@
-def topKFrequent(nums, k):
+
+def topKFrequent( nums, k):
     """
     :type nums: List[int]
     :type k: int
     :rtype: List[int]
     """
-    count = {}
+    ans = {}
+    most = 0
     for i in nums:
-        count[i] = 1 + count.get(i, 0)
-    
-    ans = [[] for i in range(len(nums))]
-    for k2, v in count.items():
-        ans[v - 1].append(k2)
-    
-    print(ans)
-    result = []
-    tmp = 0
-    for i in ans[::-1]:
-        while len(i) > 0 and tmp < k:
-            result.append(i[0])
-            i = i[1:]
-            tmp += 1
-
-    return result
+        ans[i] = 1 + ans.get(i, 0)
+        most = max(most, ans[i])
+    result = [[] for _ in range(most)]
+    for key, value in ans.items():
+        result[value - 1].append(key)
+    actual = []
+    for i in range(most - 1, -1, -1):
+        if k <= 0:
+            return actual
+        k-=len(result[i])
+        actual.extend(result[i])
+    return actual
 
 print(topKFrequent([1,1,1,2,2,3], 2))
