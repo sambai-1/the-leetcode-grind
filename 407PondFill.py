06a3598cd6a8ca55c53465c8ce360a998b762ba6
sam <sam.bai@berkeley.edu>
2025-07-17 08:16:33 -0700

    modified code, finally understand

--- 407PondFill.py
+++ 407PondFill.py
@@ -2,52 +2,75 @@ import heapq
 
 
 def trapRainWater(heightMap):
-        """
-        :type heightMap: List[List[int]]
-        :rtype: int
-        """
-        
-
-        todo = []
-        visited = set()
-        def get(x, y, curr):
-            if x < 0 or y < 0 or x >= len(heightMap) or y >= len(heightMap[0]):
-                return curr
-            if (x, y) not in visited:
-                heapq.heappush(todo, (heightMap[x][y], x, y))
-            return heightMap[x][y]
-
-        for i in range(len(heightMap[0])):
-            heapq.heappush(todo, (heightMap[0][i], 0, i))
-            visited.add((0, i))
-            if (len(heightMap) - 1, i) not in visited:
-                heapq.heappush(todo, (heightMap[len(heightMap) - 1][i], len(heightMap) - 1, i))
-                visited.add((len(heightMap) - 1, i))
-        
-        for j in range(1, len(heightMap) - 1):
-            if (j, 0) not in visited:
-                heapq.heappush(todo, (heightMap[j][0], j, 0))
-                visited.add((j, 0))
-            if (j, len(heightMap[0]) - 1) not in visited:
-                heapq.heappush(todo, (heightMap[j][len(heightMap[0]) - 1], j, len(heightMap[0]) - 1))
-                visited.add((j, len(heightMap[0]) - 1))
-        visited.clear()
-        print(todo)
-        ans = 0
-        while todo:
-            curr, x, y = heapq.heappop(todo)
-        
-            if (x, y) in visited:
-                continue
-
-            least = min(get(x, y + 1, curr), get(x, y - 1, curr), get(x + 1, y, curr), get(x - 1, y, curr))
-            ans += (least - curr) if least > curr else 0
-            print(x, y)
-            heightMap[x][y] = max(least, curr)
-
-            visited.add((x, y))
-            
-        return ans
+    """
+    :type heightMap: List[List[int]]
+    :rtype: int
+    """
+    considering = set()
+    ans = 0
     
+    def assign(x, y, curr):
+        
+        if x < 0 or y < 0 or x >= len(heightMap) or y >= len(heightMap[0]):
+            return
+        if (x, y) not in considering:
+            considering.add((x, y))
+            tmp = max(curr, heightMap[x][y])
+            heapq.heappush(todo, (tmp, x, y))
+
+    for i in range(len(heightMap[0])):
+        considering.add((0, i))
+        considering.add((len(heightMap) - 1, i))
+    for i in range(len(heightMap)):
+        considering.add((i, 0))
+        considering.add((i, len(heightMap[0]) - 1))
+    todo = []
+    for x, y in considering:
+        heapq.heappush(todo, (heightMap[x][y], x, y))
+
+    while todo:
+        height, x, y = heapq.heappop(todo)
+        
+        if height > heightMap[x][y]:
+            ans += height - heightMap[x][y]
+            heightMap[x][y] = height
+        assign(x + 1, y, heightMap[x][y])
+        assign(x, y + 1, heightMap[x][y])
+        assign(x - 1, y, heightMap[x][y])
+        assign(x, y - 1, heightMap[x][y])
+
     
 print(trapRainWater([[3,3,3,3,3],[3,2,2,2,3],[3,2,1,2,3],[3,2,2,2,3],[3,3,3,3,3]]))
+
+"""class Solution(object):
+    def trapRainWater(self, heightMap):
+        ans = 0
+        maxHeight = 0
+        considering = set()
+        for i in range(len(heightMap[0])):
+            considering.add((0, i))
+            considering.add((len(heightMap) - 1, i))
+        for i in range(len(heightMap)):
+            considering.add((i, 0))
+            considering.add((i, len(heightMap[0]) - 1))
+        todo = []
+        for x, y in considering:
+            heapq.heappush(todo, (heightMap[x][y], x, y))
+            heightMap[x][y] = -1
+
+        while todo:
+            height, x, y = heapq.heappop(todo)
+            maxHeight = max(maxHeight, height)
+            ans += (maxHeight - height)
+            
+            moves = [(x + 1, y), (x, y + 1), (x - 1, y), (x, y - 1)]
+            for r, c in moves:
+                if r < 0 or r >= len(heightMap) or c < 0 or c >= len(heightMap[0]) or heightMap[r][c] == -1:
+                    continue
+                else:
+                    heapq.heappush(todo, (heightMap[r][c], r, c))
+                    heightMap[r][c] = -1
+
+        return ans      
+
+"""
